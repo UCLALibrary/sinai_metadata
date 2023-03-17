496daaf2c95a9df6110f0a12ad08258718b86af0
Dawn Childress <kirschbombe@gmail.com>
2023-03-16 23:50:38 -0700

    remove leading zeros from del. 1; add script to remove leading zeros from folio titles

---
+++ scripts/utilities/rm_leading_zeros.py
@@ -0,0 +1,28 @@
+import os
+import csv
+import re
+
+# Prompt the user to input the directory path where the CSV files are located
+directory_path = input("Enter the directory path: ")
+
+# Define a regular expression pattern to match "f. " followed by anything up to the first number between 1 and 9
+pattern = r"(f\.\s*)([^1-9]*)([1-9])"
+
+# Loop through all CSV files in the directory
+for file_name in os.listdir(directory_path):
+    if file_name.endswith(".csv"):
+        file_path = os.path.join(directory_path, file_name)
+        # Open the CSV file for reading and writing
+        with open(file_path, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            rows = []
+            for row in reader:
+                # Remove anything between "f. " and the first number between 1 and 9 in the 'Title' column
+                row['Title'] = re.sub(pattern, r"\1\3", row['Title'])
+                rows.append(row)
+        # Write the updated rows to the CSV file
+        with open(file_path, 'w', newline='') as csvfile:
+            fieldnames = reader.fieldnames
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(rows)
