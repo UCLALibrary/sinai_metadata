7d68941a9bdbcbf7f802783388dda9cbf4525173
William L Potter <44096602+wlpotter@users.noreply.github.com>
2024-12-12 11:31:39 -0600

    Allow the reference instances and paracontents csv be optional

--- scripts/migrate-airtable-to-data-portal/migrate-airtable-to-data-portal.py
+++ scripts/migrate-airtable-to-data-portal/migrate-airtable-to-data-portal.py
@@ -963,13 +963,19 @@ except ValueError:
      print(f"'{record_type}' is not a valid type, must be one of {RECORD_TYPES}")
 
 # Get the path to a CSV of reference instances for creating bibliography records; set the index of the DataFrame as the ID column, useful for accessing by ID
-path_to_ref_instances_csv = input("Please input a path to the CSV containing the reference instances for creating the bibliography field:")
+path_to_ref_instances_csv = input("Please input a path to the CSV containing the reference instances for creating the bibliography field (or leave blank if unused):")
 # path_to_ref_instances_csv = "/Users/wpotter/Desktop/SMDP-Migration/csvs/reference_instances.csv" # testing remove this line
-ref_instances = pd.read_csv(path_to_ref_instances_csv, index_col='ID')
+if path_to_ref_instances_csv:
+    ref_instances = pd.read_csv(path_to_ref_instances_csv, index_col='ID')
+else:
+    ref_instances = None
 
-path_to_paracontents_csv = input("Please input a path to the CSV containing the paracontents info for creating the paracontents field:")
+path_to_paracontents_csv = input("Please input a path to the CSV containing the paracontents info for creating the paracontents field (or leave blank if unused):")
 # path_to_paracontents_csv = "/Users/wpotter/Desktop/SMDP-Migration/csvs/colophons_TEST.csv" # testing remove this line
-paracontents_table = pd.read_csv(path_to_paracontents_csv, index_col='ID')
+if path_to_paracontents_csv:
+    paracontents_table = pd.read_csv(path_to_paracontents_csv, index_col='ID')
+else:
+    paracontents_table = None
 
 # TBD: check that all of the columns are present, or added, for a given record type
 # Report the mismatched fields and prompt user to continue
